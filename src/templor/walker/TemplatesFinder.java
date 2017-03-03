ad0b6aebcfd338fa06165b846389d62cd053f56e
TRAN VAN BA Vu Anh Lâm <lam.tranvanba@viacesi.fr>
2017-03-03 18:39:20 -0500

    Parsing in the Templates Finder to check if the templates are good

--- src/templor/walker/TemplatesFinder.java
+++ src/templor/walker/TemplatesFinder.java
@@ -1,10 +1,15 @@
 package templor.walker;
 
+import mino.language_mino.LexerException;
+import mino.language_mino.ParserException;
+
 import templor.exception.InterpreterException;
 import templor.language_templor.*;
 import templor.language_templor.Node;
 import templor.language_templor.Walker;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,7 +32,31 @@ public class TemplatesFinder
             NStm_Create node) {
 
         visit(node.get_AddTemplate());
-        if(this.templateDef != null){
+
+        if(this.templateDef == null){
+            throw new InterpreterException("Template cannot be null", node.get_Eq());
+        }
+
+        String template = this.templateDef.getText().replaceAll("<\\{"," ").replaceAll("}>", " ");
+        StringReader reader = new StringReader(template);
+        mino.language_mino.Node syntaxTree = null;
+        try {
+            syntaxTree = new mino.language_mino.Parser(reader).parse();
+        }
+        catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+        catch (ParserException e) {
+            System.err.println("SYNTAX ERROR: " + e.getMessage() + ".");
+            System.exit(1);
+        }
+        catch (LexerException e) {
+            System.err.println("LEXICAL ERROR: " + e.getMessage() + ".");
+            System.exit(1);
+        }
+
+        if(syntaxTree != null){
             this.templates.put(node.get_TemplateName().getText(), this.templateDef);
         }
     }
@@ -52,7 +81,7 @@ public class TemplatesFinder
         if(this.templates.containsKey(template_name)){
             this.templateDef = this.templates.get(template_name);
         }else{
-            throw new InterpreterException("Template of name " + template_name + " is unknown", node.getLine(), node.getPos());
+            throw new InterpreterException("Template of name " + template_name + " is unknown", node.get_Id());
         }
     }
 
